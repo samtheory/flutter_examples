8e52f3e82e4cdd92eefabdff1c2ee56536e9600f
sajjad Alizadeh <samycrip10@gmail.com>
2021-12-27 17:58:39 +0330

    Add Animation to street_beer_page

--- lib/pages/street_beer.dart
+++ lib/pages/street_beer.dart
@@ -1,7 +1,7 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_examples/Router/router.dart';
-import 'package:flutter_examples/pages/street_beer_detail.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:auto_route/auto_route.dart';
 
@@ -19,71 +19,79 @@ class StreetBeerPage extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Icon(
-                  EvaIcons.menu2Outline,
-                  size: 30,
-                ),
-                Stack(
-                  children: [
-                    const Icon(
-                      EvaIcons.shoppingCart,
-                      size: 30,
-                    ),
-                    Positioned(
-                        top: 0,
-                        right: 0,
-                        child: Container(
-                          width: 14,
-                          height: 14,
-                          decoration: BoxDecoration(
-                              color: Colors.cyan[600],
-                              borderRadius: BorderRadius.circular(50)),
-                          child: const Center(
-                            child: Text(
-                              '2',
-                              style: TextStyle(
-                                fontSize: 10,
-                                color: Colors.white,
+            FadeInDown(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Icon(
+                    EvaIcons.menu2Outline,
+                    size: 30,
+                  ),
+                  Stack(
+                    children: [
+                      const Icon(
+                        EvaIcons.shoppingCart,
+                        size: 30,
+                      ),
+                      Positioned(
+                          top: 0,
+                          right: 0,
+                          child: Container(
+                            width: 14,
+                            height: 14,
+                            decoration: BoxDecoration(
+                                color: Colors.cyan[600],
+                                borderRadius: BorderRadius.circular(50)),
+                            child: const Center(
+                              child: Text(
+                                '2',
+                                style: TextStyle(
+                                  fontSize: 10,
+                                  color: Colors.white,
+                                ),
                               ),
                             ),
-                          ),
-                        ))
-                  ],
-                )
-              ],
+                          ))
+                    ],
+                  )
+                ],
+              ),
             ),
             Flexible(
                 child: ListView(
               children: [
                 Container(
                   margin: const EdgeInsets.only(top: 20, left: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Good morning,',
-                          style: GoogleFonts.poppins(
-                              fontSize: 20, fontWeight: FontWeight.w500)),
-                      Text('Sajjad Theory',
-                          style: GoogleFonts.poppins(
-                              fontSize: 24, fontWeight: FontWeight.bold))
-                    ],
+                  child: FadeInDown(
+                    delay: const Duration(milliseconds: 300),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('Good morning,',
+                            style: GoogleFonts.poppins(
+                                fontSize: 20, fontWeight: FontWeight.w500)),
+                        Text('Sajjad Theory',
+                            style: GoogleFonts.poppins(
+                                fontSize: 24, fontWeight: FontWeight.bold))
+                      ],
+                    ),
                   ),
                 ),
                 const SizedBox(
                   height: 30,
                 ),
-                TextField(
-                  decoration: InputDecoration(
-                      filled: true,
-                      fillColor: Colors.white,
-                      prefixIcon: const Icon(EvaIcons.searchOutline),
-                      hintText: 'search',
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide.none,
-                          borderRadius: BorderRadius.circular(50))),
+                FadeInDown(
+                  delay: const Duration(milliseconds: 400),
+                  child: TextField(
+                    decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        prefixIcon: const Icon(EvaIcons.searchOutline),
+                        hintText: 'search',
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.circular(50))),
+                  ),
                 ),
                 const SizedBox(
                   height: 20,
@@ -94,20 +102,24 @@ class StreetBeerPage extends StatelessWidget {
                     scrollDirection: Axis.horizontal,
                     itemCount: _brands.length,
                     itemBuilder: (BuildContext context, int index) {
-                      return Container(
-                        margin: const EdgeInsets.only(right: 10, left: 10),
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            color: _brandActive == index
-                                ? Colors.cyan[600]
-                                : null),
-                        child: Center(
-                          child: Text(_brands[index],
-                              style: GoogleFonts.poppins(
-                                  color: _brandActive == index
-                                      ? Colors.white
-                                      : Colors.black54)),
+                      return FadeInLeft(
+                        delay:
+                            Duration(milliseconds: 400 + (50 * index).toInt()),
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 10, left: 10),
+                          padding: const EdgeInsets.symmetric(horizontal: 20),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(50),
+                              color: _brandActive == index
+                                  ? Colors.cyan[600]
+                                  : null),
+                          child: Center(
+                            child: Text(_brands[index],
+                                style: GoogleFonts.poppins(
+                                    color: _brandActive == index
+                                        ? Colors.white
+                                        : Colors.black54)),
+                          ),
                         ),
                       );
                     },
@@ -137,97 +149,106 @@ class StreetBeerPage extends StatelessWidget {
                               heroPriceTag: heroPriceTag,
                               heroTitleTag: heroTitleTag));
                         },
-                        child: Stack(
-                          children: [
-                            Column(
-                              children: [
-                                const SizedBox(
-                                  height: 60,
-                                ),
-                                Container(
-                                  margin: const EdgeInsets.only(
-                                      left: 10, right: 30),
-                                  width: 220,
-                                  height: 300,
-                                  decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.circular(15)),
-                                ),
-                              ],
-                            ),
-                            Positioned(
-                                left: 0,
-                                right: 0,
-                                child: Hero(
-                                  tag: heroImageTag,
-                                  child: Image.asset(
-                                    _beers[index].image,
-                                    height: 260,
+                        child: FadeInLeft(
+                          delay: Duration(
+                              milliseconds: 300 + (50 * index).toInt()),
+                          child: Stack(
+                            children: [
+                              Column(
+                                children: [
+                                  const SizedBox(
+                                    height: 60,
                                   ),
-                                )),
-                            Positioned(
-                                bottom: 45,
-                                left: 35,
-                                right: 0,
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Hero(
-                                      tag: heroTitleTag,
-                                      child: SizedBox(
-                                        width: 105,
-                                        height: 20,
-                                        child: Text(_beers[index].title,
-                                            style: GoogleFonts.poppins(
-                                                fontSize: 14,
-                                                color: Colors.grey[700])),
-                                      ),
-                                    ),
-                                    Hero(
-                                      tag: heroPriceTag,
-                                      child: SizedBox(
-                                        width: 105,
-                                        height: 25,
-                                        child: Text(
-                                          _beers[index].price,
-                                          style: GoogleFonts.frederickaTheGreat(
-                                              fontSize: 22,
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )),
-                            Positioned(
-                                bottom: 0,
-                                right: 40,
-                                child: GestureDetector(
-                                  onTap: () {
-                                    print('Added To Card');
-                                  },
-                                  child: Container(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 5),
+                                  Container(
+                                    margin: const EdgeInsets.only(
+                                        left: 10, right: 30),
+                                    width: 220,
+                                    height: 300,
                                     decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(50),
-                                        color: Colors.cyan[400]),
-                                    child: Row(
-                                      children: [
-                                        const Icon(EvaIcons.plusCircleOutline),
-                                        const SizedBox(
-                                          width: 20,
-                                        ),
-                                        Text(
-                                          'Add to cart',
-                                          style: GoogleFonts.poppins(
-                                              fontWeight: FontWeight.bold),
-                                        )
-                                      ],
-                                    ),
+                                        color: Colors.white,
+                                        borderRadius:
+                                            BorderRadius.circular(15)),
                                   ),
-                                ))
-                          ],
+                                ],
+                              ),
+                              Positioned(
+                                  left: 0,
+                                  right: 0,
+                                  child: Hero(
+                                    tag: heroImageTag,
+                                    child: Image.asset(
+                                      _beers[index].image,
+                                      height: 260,
+                                    ),
+                                  )),
+                              Positioned(
+                                  bottom: 45,
+                                  left: 35,
+                                  right: 0,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Hero(
+                                        tag: heroTitleTag,
+                                        child: SizedBox(
+                                          width: 105,
+                                          height: 20,
+                                          child: Text(_beers[index].title,
+                                              style: GoogleFonts.poppins(
+                                                  fontSize: 14,
+                                                  color: Colors.grey[700])),
+                                        ),
+                                      ),
+                                      Hero(
+                                        tag: heroPriceTag,
+                                        child: SizedBox(
+                                          width: 105,
+                                          height: 25,
+                                          child: Text(
+                                            _beers[index].price,
+                                            style:
+                                                GoogleFonts.frederickaTheGreat(
+                                                    fontSize: 22,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: Colors.black),
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  )),
+                              Positioned(
+                                  bottom: 0,
+                                  right: 40,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      print('Added To Card');
+                                    },
+                                    child: Container(
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 10, vertical: 5),
+                                      decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(50),
+                                          color: Colors.cyan[400]),
+                                      child: Row(
+                                        children: [
+                                          const Icon(
+                                              EvaIcons.plusCircleOutline),
+                                          const SizedBox(
+                                            width: 20,
+                                          ),
+                                          Text(
+                                            'Add to cart',
+                                            style: GoogleFonts.poppins(
+                                                fontWeight: FontWeight.bold),
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                  ))
+                            ],
+                          ),
                         ),
                       );
                     },
